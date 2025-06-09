8d55cc78d15c7ae51d1d81a38093351f45482c64
Jaden Bertino <jaden@ossa.ai>
2025-06-08 19:10:14 -0700

    use catpuccin as theme + some pane changes

--- .config/zsh/tmux.sh
+++ .config/zsh/tmux.sh
@@ -17,6 +17,15 @@ if [ ! -d "$TPM_DIR" ]; then
   echo "TPM installed successfully"
 fi
 
+# Install Catpuccin theme
+CATPUCCIN_DIR="$HOME/.config/tmux/plugins/catppuccin/tmux"
+if [ ! -d "$CATPUCCIN_DIR" ]; then
+  echo "Installing Catpuccin theme..."
+  mkdir -p "$CATPUCCIN_DIR"
+  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git "$CATPUCCIN_DIR" > /dev/null
+  echo "Catpuccin theme installed successfully"
+fi
+
 # Aliases
 alias tm='tmux'
 alias tml='tmux list-sessions'
@@ -24,9 +33,22 @@ alias tma='~/.local/bin/tmux-sessionizer.sh'
 alias tmd='tmux detach'
 alias tmc='tmux new-session -s'
 tmk() {
+    # kill a session
     if [[ $# -eq 0 ]]; then
         tmux kill-session
     else
         tmux kill-session -t "$1"
     fi
+}
+function tat {
+  # attach to or create a new session based on the current directory name
+  name=$(basename `pwd` | sed -e 's/\.//g')
+
+  if tmux ls 2>&1 | grep "$name"; then
+    tmux attach -t "$name"
+  elif [ -f .envrc ]; then
+    direnv exec / tmux new-session -s "$name"
+  else
+    tmux new-session -s "$name"
+  fi
 }
