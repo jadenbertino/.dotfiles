5445361c084097913cf64750c14f4c0461405701
Jaden Bertino <jaden@bertinofamily.com>
2025-06-24 18:41:40 -0700

    organize + format

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+// npm i -g prettier
+// then npx prettier --write .
+// https://prettier.io/docs/en/options
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  singleAttributePerLine: true,
+};
