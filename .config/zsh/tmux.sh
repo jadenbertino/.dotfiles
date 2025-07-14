3a3ebcb5c8b17dde3aba63d4d072d4081f9d8209
Jaden-Bertino <jaden@bertinofamily.com>
2025-07-14 16:47:03 -0700

    move tmux stuff around

--- .config/zsh/tmux.sh
+++ .config/zsh/tmux.sh
@@ -1,5 +1,30 @@
-# Honestly I don't think I'll really use tmux
-# But it's here if I need it
+# Aliases
+alias tm='tmux'
+alias tml='tmux list-sessions'
+alias tma='~/.local/bin/tmux-sessionizer.sh'
+alias tmd='tmux detach'
+alias tmc='tmux new-session -s'
+alias tmka='tmux kill-server'
+tmk() {
+    # kill a session
+    if [[ $# -eq 0 ]]; then
+        tmux kill-session
+    else
+        tmux kill-session -t "$1"
+    fi
+}
+tat() {
+  # attach to or create a new session based on the current directory name
+  name=$(basename `pwd` | sed -e 's/\.//g')
+
+  if tmux ls 2>&1 | grep "$name"; then
+    tmux attach -t "$name"
+  elif [ -f .envrc ]; then
+    direnv exec / tmux new-session -s "$name"
+  else
+    tmux new-session -s "$name"
+  fi
+}
 
 # Install tmux if it doesn't exist
 source ~/.config/zsh/utils.sh
@@ -25,31 +50,3 @@ if [ ! -d "$CATPUCCIN_DIR" ]; then
   git clone -q -b v2.1.3 https://github.com/catppuccin/tmux.git "$CATPUCCIN_DIR" > /dev/null
   echo "Catpuccin theme installed successfully"
 fi
-
-# Aliases
-alias tm='tmux'
-alias tml='tmux list-sessions'
-alias tma='~/.local/bin/tmux-sessionizer.sh'
-alias tmd='tmux detach'
-alias tmc='tmux new-session -s'
-alias tmka='tmux kill-server'
-tmk() {
-    # kill a session
-    if [[ $# -eq 0 ]]; then
-        tmux kill-session
-    else
-        tmux kill-session -t "$1"
-    fi
-}
-function tat {
-  # attach to or create a new session based on the current directory name
-  name=$(basename `pwd` | sed -e 's/\.//g')
-
-  if tmux ls 2>&1 | grep "$name"; then
-    tmux attach -t "$name"
-  elif [ -f .envrc ]; then
-    direnv exec / tmux new-session -s "$name"
-  else
-    tmux new-session -s "$name"
-  fi
-}
