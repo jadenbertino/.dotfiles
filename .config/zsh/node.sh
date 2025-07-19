d6302580f43f4e7c5b3c83b18cba6080f1849042
Jaden Bertino <jaden@bertinofamily.com>
2025-07-18 17:40:55 -0700

    extract installs

--- .config/zsh/node.sh
+++ .config/zsh/node.sh
@@ -5,36 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 
 # Function to lazy load NVM - only runs once (on first call of whichever comes first: nvm, node, npm, or npx)
 load_nvm() {
-    # Remove lazy loading functions
     unset -f nvm node npm pnpm npx 2>/dev/null
-
-    # Install nvm if it doesn't exist
-    if [ ! -d "$NVM_DIR" ]; then
-        echo "Installing nvm..."
-        git clone -q https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-        (cd "$NVM_DIR" && git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`)
-        echo "Installed nvm"
-    fi
-
-    # Install node version if none exists + set default
-    if [ -n "$NVM_DIR" ] && [ -s "$NVM_DIR/nvm.sh" ]; then
-        # Install a Node version if none exist
-        if [ ! -d "$NVM_DIR/versions/node" ] || [ ! "$(ls -A $NVM_DIR/versions/node 2>/dev/null)" ]; then
-            echo "No Node.js versions found. Installing latest LTS..."
-            load_nvm
-            nvm install --lts
-        fi
-        
-        # Set default if none is set (versions should exist at this point)
-        if [ ! -f "$NVM_DIR/alias/default" ]; then
-            load_nvm
-            local latest_version=$(ls -1 "$NVM_DIR/versions/node" | tail -1)
-            nvm alias default "$latest_version" --silent 2>/dev/null
-            nvm use default --silent 2>/dev/null
-        fi
-    fi
-
-    # Load nvm
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 }
@@ -80,3 +51,34 @@ pnpm() {
 
 alias pm="pnpm"
 alias pmx="pnpm dlx"
+
+setup_node() {
+    # Install nvm if it doesn't exist
+    if [ ! -d "$NVM_DIR" ]; then
+        echo "Installing nvm..."
+        git clone -q https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+        (cd "$NVM_DIR" && git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`)
+        echo "Installed nvm"
+    fi
+
+    # Install node version if none exists + set default
+    if [ -n "$NVM_DIR" ] && [ -s "$NVM_DIR/nvm.sh" ]; then
+
+        # Install a Node version if none exist
+        if [ ! -d "$NVM_DIR/versions/node" ] || [ ! "$(ls -A $NVM_DIR/versions/node 2>/dev/null)" ]; then
+            load_nvm
+            echo "No Node.js versions found. Installing latest LTS..."
+            nvm install --lts
+        fi
+        
+        # Set default if none is set (versions should exist at this point)
+        if [ ! -f "$NVM_DIR/alias/default" ]; then
+            load_nvm
+            local latest_version=$(ls -1 "$NVM_DIR/versions/node" | tail -1)
+            nvm alias default "$latest_version" --silent 2>/dev/null
+            nvm use default --silent 2>/dev/null
+        fi
+    fi
+}
+
+setup_node
