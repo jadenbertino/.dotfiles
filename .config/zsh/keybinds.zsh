d516fbba6b295783d05db1abcd57f4a9fc6aa427
Jaden Bertino <jaden@bertinofamily.com>
2025-09-22 10:52:32 -0700

    alt j l term keybinds

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -7,3 +7,6 @@ export FZF_CTRL_T_COMMAND="" # disable fzf ctrl + t
 export FZF_ALT_C_COMMAND="" # disable fzf alt + c
 bindkey "^[[1;5C" forward-word # ctrl + right arrow
 bindkey "^[[1;5D" backward-word # ctrl + left arrow'
+
+bindkey '^[j' backward-char     # Alt+j — move backward one character
+bindkey '^[l' forward-char   # Alt+l — move forward one character
